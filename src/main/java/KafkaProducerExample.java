3615c0197a8b162fe98df16e24bfdcc1bfad616e
mikelhpdatke <mikelhpdatke@gmail.com>
2018-06-26 18:07:40 -0700

    Kafka Compression

---
+++ src/main/java/KafkaProducerExample.java
@@ -0,0 +1,60 @@
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.io.File;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class KafkaProducerExample {
+	private final static String TOPIC = "rawdata";
+    private final static String BOOTSTRAP_SERVERS =
+            "localhost:9092";
+    
+    private static Producer<Long, String> createProducer() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                                            BOOTSTRAP_SERVERS);
+        //props.put(ProducerConfig.COMPRESSION_TYPE_CONFIG, "gzip");
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                                        LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                                    StringSerializer.class.getName());
+        return new KafkaProducer<>(props);
+    }
+    
+    static void runProducer(final int sendMessageCount) throws Exception {
+        final Producer<Long, String> producer = createProducer();
+        long time = System.currentTimeMillis();
+
+        try {
+        	Scanner scanner = new Scanner(new File("/home/tom/tcpdump.log"));
+        	String s = null;
+            while (scanner.hasNextLine()) {
+            	s = scanner.nextLine();
+                final ProducerRecord<Long, String> record =
+                        new ProducerRecord<>(TOPIC, (long) System.currentTimeMillis(),
+                                    s);
+
+                RecordMetadata metadata = producer.send(record).get();
+
+                long elapsedTime = System.currentTimeMillis() - time;
+                System.out.printf("sent record(key=%s value=%s) " +
+                                "meta(partition=%d, offset=%d) time=%d\n",
+                        record.key(), record.value(), metadata.partition(),
+                        metadata.offset(), elapsedTime);
+
+            }
+            
+        } finally {
+            producer.flush();
+            producer.close();
+        }
+    }
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		runProducer(5);
+	}
+
+}
